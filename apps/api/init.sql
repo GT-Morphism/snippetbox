bf316ae626d35b4f71fe75e03b179fd099357790
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2025-04-22 19:26:00 +0200

    chore(api:db): escape string in postgres init

--- apps/api/init.sql
+++ apps/api/init.sql
@@ -14,19 +14,19 @@ CREATE INDEX idx_snippets_created_at ON snippets (created_at);
 
 INSERT INTO snippets (title, content, expires_at) VALUES (
   'An old silent pond',
-  'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō',
+  E'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō',
   CURRENT_TIMESTAMP + INTERVAL '365 day'
 );
 
 INSERT INTO snippets (title, content, expires_at) VALUES (
   'Over the wintry forest',
-  'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki',
+  E'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki',
   CURRENT_TIMESTAMP + INTERVAL '365 day'
 );
 
 INSERT INTO snippets (title, content, expires_at) VALUES (
   'First autumn morning',
-  'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo',
+  E'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo',
   CURRENT_TIMESTAMP + INTERVAL '7 day'
 );
 
