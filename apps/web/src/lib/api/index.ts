2793aa4c604d29796d9fb066e5354396a21dafd7
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2025-04-25 00:32:21 +0200

    chore(task:web): use env variable for api base url

--- apps/web/src/lib/api/index.ts
+++ apps/web/src/lib/api/index.ts
@@ -1,5 +1,6 @@
 import createClient from "openapi-fetch";
 import type { paths } from "./schema.d.ts";
+import { PUBLIC_API_BASE_URL } from "$env/static/public";
 
-const client = createClient<paths>({ baseUrl: "http://localhost:4000/" });
+const client = createClient<paths>({ baseUrl: PUBLIC_API_BASE_URL });
 export default client;
